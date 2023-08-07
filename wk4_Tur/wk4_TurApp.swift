eab7f8ab542253b95b7778f2479a4fc76aec34b5
RuiChengLin <s3751039@student.rmit.edu.au>
2023-08-07 17:40:39 +1000

    Initial Commit

---
+++ wk4_Tur/wk4_TurApp.swift
@@ -0,0 +1,17 @@
+//
+//  wk4_TurApp.swift
+//  wk4_Tur
+//
+//  Created by 林锐城 on 7/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct wk4_TurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
